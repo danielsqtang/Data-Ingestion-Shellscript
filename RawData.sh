7b7ddfded7f45332f46a22c09e8fe272919242e2
Daniel Tang <danielsqtang@gmail.com>
2018-03-01 15:54:23 -0500

    Update RawData.sh

--- RawData.sh
+++ RawData.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #Sqoop import Products, Customers, Orders tables from northwind database. 
 sqoop import --connect jdbc:mysql://52.207.2.108/northwind --username temp --password temp --target-dir /user/daniel/projectscript/NorthwindProducts --m 1 --table Products
-#sqoop import --connect jdbc:mysql://52.207.2.108/northwind --username temp --password temp --target-dir /user/daniel/projectscript/NorthwindCustomers --m 1 --table Customers
-sqoop import --connect jdbc:mysql://52.207.2.108/northwind --username temp --password temp --delete-target-dir --target-dir /user/daniel/projectscript/NorthwindCustomers --m 1 \
+sqoop import --connect jdbc:mysql://52.207.2.108/northwind --username temp --password temp --target-dir /user/daniel/projectscript/NorthwindCustomers --m 1 \
 --query "select CustomerID, CompanyName, ContactName, ContactTitle, Replace(Address, ',', ''), City, Region, PostalCode, Country, Phone, Fax from Customers where \$CONDITIONS"
 sqoop import --connect jdbc:mysql://52.207.2.108/northwind --username temp --password temp --target-dir /user/daniel/projectscript/NorthwindOrderDetails --m 1 --table 'OrderDetails'
 sqoop import --connect jdbc:mysql://52.207.2.108/northwind --username temp --password temp --target-dir /user/daniel/projectscript/NorthwindOrders --m 1 --table Orders
