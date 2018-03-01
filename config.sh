150bec5d21a7a0b4e7ef0b4459491fc64a8043bd
Daniel Tang <danielsqtang@gmail.com>
2018-03-01 17:05:27 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
-MYSQL = mysql://52.207.2.108
-DATABASE = 'northwind'
-USERNAME = 'temp'
-PASSWORD = 'temp'
+MYSQL = "mysql://52.207.2.108"
+DATABASE = "northwind"
+USERNAME = "temp"
+PASSWORD = "temp"
 TABLENAMES = ['Products','Customers','Orders','OrderDetails']
 
