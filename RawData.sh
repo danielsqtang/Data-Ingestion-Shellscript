a1f33b4973ba718a934640b0ef7f9697f0b7f9e2
Daniel Tang <danielsqtang@gmail.com>
2018-03-01 16:13:54 -0500

    Update RawData.sh

--- RawData.sh
+++ RawData.sh
@@ -4,7 +4,9 @@ sqoop import --connect jdbc:mysql://52.207.2.108/northwind --username temp --pas
 sqoop import --connect jdbc:mysql://52.207.2.108/northwind --username temp --password temp --target-dir /user/daniel/projectscript/NorthwindCustomers --m 1 \
 --query "select CustomerID, CompanyName, ContactName, ContactTitle, Replace(Address, ',', ''), City, Region, PostalCode, Country, Phone, Fax from Customers where \$CONDITIONS"
 sqoop import --connect jdbc:mysql://52.207.2.108/northwind --username temp --password temp --target-dir /user/daniel/projectscript/NorthwindOrderDetails --m 1 --table 'OrderDetails'
-sqoop import --connect jdbc:mysql://52.207.2.108/northwind --username temp --password temp --target-dir /user/daniel/projectscript/NorthwindOrders --m 1 --table Orders
+sqoop import --connect jdbc:mysql://52.207.2.108/northwind --username temp --password temp --target-dir /user/daniel/projectscript/NorthwindOrders --m 1 \
+--query "select OrderID, CustomerID, EmployeeID, OrderDate, RequiredDate, ShippedDate, ShipVia, Freight, ShipName, Replace(ShipAddress, ',', ''), ShipCity, ShipRegion, ShipPostalCode, ShipCountry from Orders where \$CONDITIONS"
+
 #Use variables & loop
 source config.sh
 for i in range(len($TABLENAMES)): sqoop import --connect jdbc:$MYSQL/$DATABASE --username $USERNAME --password $PASSWORD --target-dir /user/daniel/projectscript/$DATABASE/$TABLENAMES[i] --m 1 --table $TABLENAMES[i]
