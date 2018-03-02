94e4452873208718173be6da90be4cba7688a88c
Daniel Tang <danielsqtang@gmail.com>
2018-03-02 12:54:14 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,5 +2,21 @@ MYSQL="mysql://52.207.2.108"
 DATABASE="northwind"
 USERNAME="temp"
 PASSWORD="temp"
-TABLENAMES=("Products" "Customers" "Orders" "OrderDetails")
+TABLENAMES=("Orders" "OrderDetails" "Customers" "Products")
+EQUIJOINCOLUMNS=("OrderID" "CustomerID" "ProductID" "UnitPrice")
+FLATTENEDTABLENAME="NorthwindFlattened"
 
+//ONGOING
+flatten.sh
+Flattened = Orders.join(OrderDetails, ["OrderID"]).join(Customers, ["CustomerID"]).join(Products,["ProductID", "UnitPrice"])
+
+$FLATTENEDTABLENAME = $TABLENAME[0]\
+.join($TABLENAMES[1], ["$EQUIJOINCOLUMNS[0]"])\
+.join($TABLENAMES[2], ["$EQUIJOINCOLUMNS[1]"])\
+.join($TABLENAMES[3], ["$EQUIJOINCOLUMNS[2], $EQUIJOINCOLUMNS[3]"])
+
+
+Flattened.createOrReplaceTempView("tempFlattened")
+FlattenedFinal = sqlContext.sql("select distinct * from tempFlattened")
+
+FlattenedFinal.write.save('/user/daniel/projectscript/NorthwindFlattened', format='csv', mode='overwrite')
