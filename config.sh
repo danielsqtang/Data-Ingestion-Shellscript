4b035d4084838ab5eb6a7b71b1525ce9411e70ef
Daniel Tang <danielsqtang@gmail.com>
2018-03-05 13:51:20 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,6 +6,7 @@ TABLENAMES=("Orders" "OrderDetails" "Customers" "Products")
 EQUIJOINCOLUMNS=("OrderID" "CustomerID" "ProductID" "UnitPrice")
 FLATTENEDTABLENAME="NorthwindFlattened"
 TEMPFLATTENEDTABLENAME="tempNorthwindFlattened"
+FACTTABLENAME="Fact"
 
 
 
