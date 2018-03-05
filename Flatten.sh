057524575edf34bbfd46b00097abceff8a596d8e
Daniel Tang <danielsqtang@gmail.com>
2018-03-05 15:31:19 -0500

    Update Flatten.sh

--- Flatten.sh
+++ Flatten.sh
@@ -20,12 +20,12 @@ OrderDetails = hive_context.table("NorthwindOrderDetails")
 Customers = hive_context.table("NorthwindCustomers")
 Products = hive_context.table("NorthwindProducts")
 #Join Tables
-#Flattened = Orders.join(OrderDetails, ["OrderID"]).join(Customers, ["CustomerID"]).join(Products,["ProductID", "UnitPrice"])
+#Flattened = Orders.join(OrderDetails,["OrderID"]).join(Customers,["CustomerID"]).join(Products,["ProductID","UnitPrice"])
 
-$FLATTENEDTABLENAME = ($TABLENAME[0])\
-.join($TABLENAMES[1], ["$EQUIJOINCOLUMNS[0]"])\
-.join($TABLENAMES[2], ["$EQUIJOINCOLUMNS[1]"])\
-.join($TABLENAMES[3], ["$EQUIJOINCOLUMNS[2], $EQUIJOINCOLUMNS[3]"])
+$FLATTENEDTABLENAME = ${TABLENAMES[0]}\
+.join(${TABLENAMES[1]},["${EQUIJOINCOLUMNS[0]}"])\
+.join(${TABLENAMES[2]},["${EQUIJOINCOLUMNS[1]}"])\
+.join(${TABLENAMES[3]},["${EQUIJOINCOLUMNS[2]}","${EQUIJOINCOLUMNS[3]}"])
 
 #Cleanse duplicate row data
 $FLATTENEDTABLENAME.dropDuplicates()
@@ -35,8 +35,8 @@ $FLATTENEDTABLENAME.createOrReplaceTempView("$TEMPFLATTENEDTABLENAME")
 Query = sqlContext.sql("select * from $TEMPFLATTENEDTABLENAME")\
 
 #save to hdfs as csv
-$FLATTENEDTABLENAME.write.save('/user/daniel/projectscript/$FLATTENEDTABLENAME', format='csv', mode='overwrite')
-
+$FLATTENEDTABLENAME.write.save('/user/daniel/projectscript/$FLATTENEDTABLENAME',format='csv',mode='overwrite')
+NorthwindFlattened.write.save('/user/daniel/projectscript/NorthwindFlattened',format='csv',mode='overwrite')
 #Create hive table from DF (Permission error here)
 #FlattenedFinal.createOrReplaceTempView("tempFlattenedFinal") 
 #sqlContext.sql("create table NorthwindFlattened as select * from tempFlattenedFinal");
